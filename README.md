9ee58b1b038aa13f4ed86859842199bb2b04f8dc
unknown <mfvl.1186@gmail.com>
2022-03-20 20:26:28 -0300

    .

---
+++ README.md
@@ -0,0 +1,163 @@
+<div id="volver"></div>
+
+
+<br />
+<div align="center">
+
+
+<h1 align="center"><b>Sprint</b></h1>
+<h2 align="center"> Desarrollo Final del Módulo 4</h2>
+<img src="https://img.freepik.com/foto-gratis/programador-desarrollo-desarrollo-equipos-diseno-sitios-web-tecnologias-codificacion-que-trabajan-oficina-compania-software_18497-1253.jpg?size=626&ext=jpg" alt="Logo" width="330px" height="50%">
+
+
+
+<b>Autores</b>
+Francisco Astete - Patricio Garrido - Javier Espinoza - María-Fernanda Villalobos 
+<br />
+
+<br />
+GitHub: https://github.com/PanchoAste
+GitHub: https://github.com/patricio-dsgn
+GitHub:
+GitHub: https://github.com/Marifer1111
+<br />
+<br />
+
+  <p align="center">
+    CONTEXTO
+    <br/>
+        La contingencia mundial de los últimos 2 años ha incrementado la adquisición de servicios y productos de forma online. 
+        Sin duda, esta nueva forma de comercializar es la nueva normalidad para muchos clientes y comerciantes, favoreciendo 
+        en gran medida a las Starups y PyMes.
+    </summary>
+    <br />
+    <br />
+    <a href="https://github.com/Marifer1111/Ejercicios_Bootcamp/tree/main/M3/Enviados/M3-Sprint-MFVL"><strong>Acceso a Documentos »</strong></a>
+    <br />
+    <br />
+ 
+  </p>
+</div>
+
+<details>
+  <summary>Indice</summary>
+  <ol>
+    <li>
+      <a href="#acerca-del-programa">Acerca del Programa</a>
+      <ul>
+        <li><a href="#lenguaje-de-programación">Lenguaje de programación </a></li>
+      </ul>
+    </li>
+    <li><a href="#uso">Uso</a></li>
+    <li><a href="#referencias">Referencias</a></li>
+  </ol>
+</details>
+
+
+## Acerca del Programa
+
+<summary> 
+    Este proyecto tiene como fin encontrar soluciones a problemas generados por el e-commerce emergente. Para ello fue necesario desarrollar una solución tecnológica que cubra los 
+    procesos de negocio y proponer una mejora en la gestión, el control, la seguridad, y 
+    disponibilidad de información para el negocio y sus clientes. 
+    El sistema permite presentar <b>productos</b> <b>tomar pedidos</b> y <b>hacer seguimiento</b> de estos y la <b>gestión de clientes</b> Además, genera reportes y estadísticas que ayudan a tomar decisiones y mejoran el rendimiento de las empresas, considerando la cantidad de clientes, y la demanda de éstos. 
+    </summary>
+
+
+### Lenguaje de programación
+
+* [Python](https://www.python.org/)
+
+<p align="left"><a href="#volver">Volver</a></p>
+
+
+
+## Uso
+
+Este programa cuenta con 5 archivos: Bodega,Clientes y Envíos, más 2 menús con drive code para Bodega y Clientes.
+<details>
+  <summary><b>Manejo de bodega</b></summary>
+  <ul>
+    <li><a >Main_Bodega.py inicia el programa de Bodega.py </a></li>
+    <li><a>Información de productos en una bodega virtual.</a></li>
+    <li><a>Funciones:</a></li>
+        <ol>
+              <li><a>Sumar y disminuir el número de unidades por producto.</a></li>
+              <li><a>Agregar nuevos productos. </a></li>
+              <li><a>Quitar productos de la bodega virtual.</a></li>
+              <li><a>Mostrar inventario.</a></li>
+              <li><a>Quitar productos de la bodega virtual.</a></li>
+              <li><a>Verificar si un producto tiene menos de 400 unid.</a></li>
+        </ol>
+  </ul>
+</details>
+<div align="center">
+ <img src="Images/MenuBodega.png" width="400px" height="400px">
+ 
+ <img src="Images/Bodega.png" width="400px" height="400px">
+</div>
+<br />
+<details>
+  <summary><b>Información clientes</b></summary>
+  <ul>
+    <li><a>Main_Clientes.py inicia el programa de Clientes.py </a></li>
+    <li><a>Crear base de datos con información de clientes.</a></li>
+    <li><a>Funciones:</a></li>
+        <ol>
+              <li><a>Agregar nuevos clientes.</a></li>
+              <li><a>Eliminar clientes según nombre de usuario. </a></li>
+              <li><a>Comprar.</a></li>
+              <li><a>Mostrar toda la información por cliente al Empleado.</a></li>
+              <li><a>Mostrar inventario.</a></li>              
+        </ol>
+  </ul>
+</details>
+<div align="center">
+<img src="Images/MenuClientes.png " width="400px" height="400px">
+
+<img src="Images/Clientes.png" width="400px" height="400px">
+</div>
+ <br />
+<details>
+  <summary><b>Sistema de envío</b></summary>
+  <ul>
+    <li><a>Envios.py se inicia desde Clientes.py </a></li>
+    <li><a>Crear base de datos con información de clientes.</a></li>
+    <li><a>Funciones:</a></li>
+        <ol>
+              <li><a>Pregunta qué tipo de envío es necesario.</a></li>
+                  <ul>
+                      <li><a>Largo</a></li>
+                      <li><a>Corto</a></li>
+                  </ul>
+              <li><a>Enviar compra a respectiva Bodega según tipo de envío.</a></li>
+              <li><a>Verificar que cada bodega no supere las 500 unidades.</a></li>            
+        </ol>
+  </ul>
+</details>
+<div align="center">
+ <img src="Images/Envios.png" width="400px" height="400px">
+</div>
+<br />
+<p align="left"><a href="#volver">Volver</a></p>
+
+
+
+## Referencias
+
+* [Real Python](https://realpython.com/)
+* [GeeksforGeeks](https://www.geeksforgeeks.org/)
+* [Programiz](https://www.programiz.com/)
+
+<p align="left"><a href="#volver">Volver</a></p>
+
+
+
+
+
+
+
+<!--[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]-->
+
+<!--[![Product Name Screen Shot][product-screenshot]](https://example.com)
---
+++ dalecolor.py
@@ -0,0 +1,218 @@
+#format to string
+import math
+
+
+def f(txt, a = None, b = None, c = None):
+    style_font = { "normal" : 0, "bold" : 1, "weak" : 2, "italic" : 3, "underline" : 4, "inverse" : 5, "hide" : 6, "strikeout" : 7 }
+    color_font = { "black" : 30, "red" : 31, "green" : 32, "yellow" : 33, "blue" : 34, "purple" : 35, "cyan" : 36, "white" : 37 }
+    color_background = { "BLACK" : 40, "RED" : 41, "GREEN" : 42, "YELLOW" : 43, "BLUE" : 44, "PURPLE" : 45, "CYAN" : 46, "WHITE" : 47 }
+    
+    clean = '\033[0;00m'
+
+    if a == None and b == None and c == None: 
+        out = (f"\033[0;37m{txt}{clean}")
+
+    elif a != None and b == None and c == None: 
+        if a in style_font.keys(): v1 = str(style_font[a])+';37'
+        elif a in color_font.keys(): v1 = '0;'+str(color_font[a])
+        elif a in color_background.keys(): v1 = '0;'+str(color_background[a])
+        
+        out = (f"\033[{v1}m{txt}{clean}")
+        
+    elif a != None and b != None and c == None:
+        if a in style_font.keys(): v1 = style_font[a]
+        elif a in color_font.keys(): v1 = color_font[a]
+        elif a in color_background.keys(): v1 = color_background[a]
+
+        if b in style_font.keys(): v2 = style_font[b]
+        elif b in color_font.keys(): v2 = color_font[b]
+        elif b in color_background.keys(): v2 = color_background[b]
+
+        out = (f"\033[{v1};{v2}m{txt}{clean}")
+
+    elif a != None and b != None and c != None:
+        if a in style_font.keys(): v1 = style_font[a]
+        elif a in color_font.keys(): v1 = color_font[a]
+        elif a in color_background.keys(): v1 = color_background[a]
+
+        if b in style_font.keys(): v2 = style_font[b]
+        elif b in color_font.keys(): v2 = color_font[b]
+        elif b in color_background.keys(): v2 = color_background[b]
+
+        if c in style_font.keys(): v3 = style_font[c]
+        elif c in color_font.keys(): v3 = color_font[c]
+        elif c in color_background.keys(): v3 = color_background[c]
+
+        out = (f"\033[{v1};{v2};{v3}m{txt}{clean}")
+
+
+    return out
+
+# print testing - - - - -
+# print(f('hola !!!'))
+# print(f('hola','RED'))
+# print(f('hola','italic','GREEN'))
+# print(f('hola','cyan','weak'))
+# print(f('hola','red', '',''))
+# print(f('hola','', 'bold','RED'))
+
+
+
+
+
+def jumbo(txt,size=1):
+    l = len(txt)
+
+    block = ""
+
+    if size == 1:
+        block += "\n------"+("-"*l)
+        block += "\n|"+("  ")+txt+("  ")+"|"
+        block += "\n------"+("-"*l)
+    elif size == 2:
+        block += "\n------"+("-"*l)
+        block += "\n|"+("  ")+(" "*l)+("  ")+"|"
+        block += "\n|"+(" "*2)+txt+(" "*2)+"|"
+        block += "\n|"+("  ")+(" "*l)+("  ")+"|"
+        block += "\n------"+("-"*l)
+    elif size == 3:
+        block += "\n------------"+("-"*l)
+        block += "\n|"+(" "*5)+(" "*l)+(" "*5)+"|"
+        block += "\n|"+(" "*5)+(" "*l)+(" "*5)+"|"
+        block += "\n|"+(" "*5)+txt+(" "*5)+"|"
+        block += "\n|"+(" "*5)+(" "*l)+(" "*5)+"|"
+        block += "\n|"+(" "*5)+(" "*l)+(" "*5)+"|"
+        block += "\n------------"+("-"*l)
+    
+    
+    
+    
+    return block
+
+
+def table(list, head = None, head_style = None, style = None, align = None, padding = None, margin = None):
+    length_cols = {}
+
+
+    # largos primera fila
+    c = 0
+    for col in list[0]:
+        length_cols[c] = len(col)
+        c += 1
+
+    for row in list:
+        c = 0
+        for col in row:
+            if len(col) > length_cols[c]:
+                length_cols[c] = len(col)
+            c += 1
+
+    if head != None:
+        c = 0
+        for row in range(len(length_cols)):
+            # print(c)
+            # c = 0
+            # for col in row:
+            if length_cols[c] < len(head[c]):
+                length_cols[c] = len(head[c])
+            c += 1
+
+        # length_elem = 0
+        # for col in row:
+        #     length_cols
+        #     if len(col) > length_elem:
+        #         length_col = length_elem
+        # print(x)
+
+    
+    # if margin == ' ':
+    #     sep = f(' ', 'normal', 'white', None)
+
+    # length_cols2 = {}
+    # print(length_cols)
+    # for x in range(len(length_cols)):
+    #     if length_cols[x]%2 != 0:
+    #         length_cols2[x] = length_cols[x]+1
+    #     else:
+    #         length_cols2[x] = length_cols[x]
+
+    # print(length_cols2)
+
+
+
+    margin_str = margin
+    padding_str = padding
+
+    # print(head)
+    if head != None:
+        row_temp = ''
+        count_col = 0
+        for c in head:
+            mult = length_cols[count_col]-len(c)
+            xs = ''
+            
+            if count_col == len(length_cols)-1: margin_str = ''
+
+            if align == None or align == 'left':
+                space = ('.'*mult)
+                row_temp += f(padding_str+(c+space+padding_str),head_style[0] ,head_style[1],head_style[2])+margin_str
+            elif align == 'rigth':
+                space = ('.'*mult)
+                row_temp += f(padding_str+(space+c+padding_str),head_style[0] ,head_style[1],head_style[2])+margin_str
+            elif align == 'center':
+                flag1 = len(c)%2 != 0 and length_cols[count_col]%2 != 0
+                flag2 = len(c)%2 == 0 and length_cols[count_col]%2 == 0
+                if flag1 == False and flag2 == False: xs = ' '
+                space = (' '*math.floor(mult/2))
+                row_temp += f((padding_str+space+c+space+padding_str+xs), head_style[0], head_style[1], head_style[2])+margin_str
+            count_col += 1
+        print(row_temp)
+
+
+    
+
+    for row in list:    
+        row_temp = ''
+        count_col = 0
+
+        margin_str = margin
+        padding_str = padding
+
+        for c in row:
+            mult = length_cols[count_col]-len(c)
+            xs = ''
+
+            if count_col == len(length_cols)-1: margin_str = ''
+
+            if align == None or align == 'left':
+                space = (' '*mult)
+                row_temp += f((padding_str+c+space+padding_str), style[0], style[1], style[2])+margin_str
+            elif align == 'rigth':
+                space = (' '*mult)
+                row_temp += f((padding_str+space+c+padding_str), style[0], style[1], style[2])+margin_str
+            elif align == 'center':
+                flag1 = len(c)%2 != 0 and length_cols[count_col]%2 != 0
+                flag2 = len(c)%2 == 0 and length_cols[count_col]%2 == 0
+                if flag1 == False and flag2 == False: xs = ' '
+                space = (' '*math.floor(mult/2))
+                row_temp += f((padding_str+space+c+space+padding_str+xs), style[0], style[1], style[2])+margin_str
+            count_col += 1
+        print(row_temp)
+
+
+def tof(x, color1, color2):
+    if x == True:
+        print(f(x, color1))
+    else:
+        print(f(x, color2))
+
+
+
+
+def clear():
+    print(chr(27)+'[2j')
+    print('\033c')
+    print('\x1bc')
+
+
+
