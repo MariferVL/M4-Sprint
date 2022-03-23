3e8a28b650e0db5de29e9636339667d08bfcb465
patricio-dsgn <patricio.ngg@gmail.com>
2022-03-23 20:56:15 -0300

    comment main

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # SPRINT 4
-
+# para darle color a la terminal y darle formato
 import dalecolor as dc
 import random
-
+# limpiar la terminal
 dc.clear()
 # print("\n\n\n\n\n\n\n")
 
 
 def total(bod_stock):
+    '''
+    suma to do el stock del parametro
+    bod_stock: diccionario con los productos y sus stock
+    return: total del stock
+    '''
     total = 0 
     for i in bod_stock:
         total += bod_stock[i]
@@ -15,6 +20,10 @@ def total(bod_stock):
 
 
 class Bodega:
+    '''
+    clase que define la clase bodega
+    '''
+
     def __init__(self, id, nombre, total_stock, proveedores, stock, transferencias = None):
         self.id = id
         self.nombre = nombre
@@ -31,10 +40,9 @@ class Bodega:
         if proveedor_eliminar in self.proveedores:
             self.proveedores.remove(proveedor_eliminar)
 
-    transf_tipo_ = ''
-    transf_cantidad_ = 0
 
     def transferencia_bodega(self, inst_bodega_destino, transferencia = None):
+        # iniciar las variables
         key = ''
         value = ''
         if transferencia is None:
@@ -60,41 +68,50 @@ class Bodega:
         # print(f"---------{key} : {value}---------")
 
     def transferencia_mostrar_tipo(self):
+        ''' muestra el tipo de transferencia'''
         last = self.transferencias[-1]
         return(last[0])
 
     def transferencia_mostrar_cantidad(self):
+        '''muestra la ultima transferencia'''
         last = self.transferencias[-1]
         return(last[1])
 
     def mostrar_total_de_productos_en_bodega(self):
+        '''muestra el total de productos en la bodega'''
         x = total(self.stock)
         return (f"total de productos en bodega: {x}")
 
 
 class Proveedor:
+    ''' clase que define el proveedor'''
     def __init__(self, id, nombre, tipo):
         self.id = id
         self.nombre = nombre
         self.tipo = tipo
 
     def inscripcion_en_bodega(self, bodega):
+        '''funcion que inscribe en una bodega'''
         if self.id not in bodega.proveedores:
             bodega.proveedores.append(self.id)
 
     def mod_producto(self, nuevo_tipo):
+        '''funcion que modifica el tipo de producto'''
         self.tipo = nuevo_tipo
 
 
 
 class User:
+    ''' clase que define el usuario'''
     def __init__(self, id, nombre, tipo):
         self.id = id
         self.nombre = nombre
+        # define el tipo de usuario
         self.tipo = tipo
 
     def consulta(self, bodega):
         consulta = []
+        # segun el tipo de usuario muestra un resultado diferente
         if self.tipo == 'administrador':
             consulta.append(len(bodega.proveedores))
             consulta.append(bodega.stock)
@@ -103,6 +120,7 @@ class User:
 
         return consulta
 
+# se instancia los proveedores
                      # id   nombre   tipo
 proveedor_1 = Proveedor(1, 'China', 'pc')
 proveedor_2 = Proveedor(2, 'Korea', 'phone')
@@ -110,6 +128,7 @@ proveedor_3 = Proveedor(3, 'Japón', 'tablet')
 proveedor_4 = Proveedor(4, 'Taiwán', 'watch')
 proveedor_5 = Proveedor(5, 'Vietnam', 'tv')
 
+# se crea info para instanciar bodega
 
 # id, nombre, total_stock, proveedores, stock
 bod = {
@@ -118,14 +137,14 @@ bod = {
     3 : ['C', [proveedor_4.id, proveedor_5.id], {'watch' : 600, 'tv' : 400}]
 }
 
-
+# se instancian bodegas
 
                 # id nombre,    total_stock       proveedores    stock
 bodega_a = Bodega(1, bod[1][0], total(bod[1][2]), bod[1][1], bod[1][2])
 bodega_b = Bodega(2, bod[2][0], total(bod[2][2]), bod[2][1], bod[2][2])
 bodega_c = Bodega(3, bod[3][0], total(bod[3][2]), bod[3][1], bod[3][2])
 
-
+# se instancian los usuarios
             # id   nombre    tipo
 user_0 = User(35, 'andres', 'administrador')
 user_1 = User(235, 'beatriz', 'operario')
@@ -133,7 +152,7 @@ user_2 = User(861, 'camilo', 'operario')
 
 
 # - - - - - - - - - - - - - - -
-
+# testear las funciones
 print(dc.jumbo(f"instaciación usuarios, bodegas, proveedores"))
 
 print()
